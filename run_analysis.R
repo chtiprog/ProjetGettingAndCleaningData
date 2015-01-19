c639af618d3f06e098f5f44b609ca6bee114c3af
spellart <chtiprog@free.fr>
2015-01-19 15:27:42 -0200

    Add script to extract only mean and std, customization
    Create data frames which extracts only measurements on the mean and
    standard deviation
    Customize column names for the firsts data frames

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,18 @@
-# Merges the training and the test sets to create one data set.
+# Create the data frames with the given data set
 testSet <- read.table("X_test.txt")
 trainSet <- read.table("X_train.txt")
 features <- read.table("features.txt")
+# Name the column with the names given in the features file
 colonnesNames <- features$V2
 colnames(testSet) <- colonnesNames
 colnames(trainSet) <- colonnesNames
+# Custome column'names to distinguish the test and train datasets
+colnames(testSet) <- paste("test", colnames(testSet), sep = "-")
+colnames(trainSet) <- paste("train", colnames(trainSet), sep = "-")
+
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+setWithMeanTestSet <- testSet[ , grepl("mean" , names(testSet))]
+setWithSdTestSet <- testSet[ , grepl("std", names(testSet))]
+setWithMeanTrainSet <- trainSet[ , grepl("mean" , names(trainSet))]
+setWithSdTrainSet <- trainSet[ , grepl("std", names(trainSet))]
+
